9665d67405a39caa7fd3f2dc374026421b704f60
duje nizic <duje.nizic4@gmail.com>
2024-12-09 23:12:52 +0100

    Initial commit

---
+++ BookerApp/src/main/java/com/example/BookerApp/BookerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookerAppApplication.class, args);
+	}
+
+}
