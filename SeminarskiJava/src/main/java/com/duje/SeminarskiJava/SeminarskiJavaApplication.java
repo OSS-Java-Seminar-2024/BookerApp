529ac0345ba78b22e8b280c637f8f1b2eac52d5c
duje nizic <duje.nizic4@gmail.com>
2024-12-09 23:37:51 +0100

    Initial commit

---
+++ SeminarskiJava/src/main/java/com/duje/SeminarskiJava/SeminarskiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.duje.SeminarskiJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeminarskiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeminarskiJavaApplication.class, args);
+	}
+
+}
---
+++ SeminarskiJava/src/test/java/com/duje/SeminarskiJava/SeminarskiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duje.SeminarskiJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminarskiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
