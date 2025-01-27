691f9a1836af9fc4dce4713d3f2bb49f5de52497
DujeNizic <147092639+DujeNizic@users.noreply.github.com>
2025-01-27 19:06:20 +0100

    Delete projekt directory

--- projekt/src/main/java/com/duje/projekt/Repository/TripRepository.java
+++
@@ -1,9 +0,0 @@
-package com.duje.projekt.Repository;
-
-import com.duje.projekt.model.TripModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface TripRepository extends JpaRepository<TripModel,Integer> {
-}
