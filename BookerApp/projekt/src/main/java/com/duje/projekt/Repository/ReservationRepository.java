3b1c569c475169667575c700e813777fc55985d5
DujeNizic <147092639+DujeNizic@users.noreply.github.com>
2025-01-31 00:34:43 +0100

    Delete BookerApp directory

--- BookerApp/projekt/src/main/java/com/duje/projekt/Repository/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.duje.projekt.Repository;
-
-import com.duje.projekt.model.ReservationModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<ReservationModel,Integer> {
-}
